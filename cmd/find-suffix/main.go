7c3ffb3c742a217b7612a1cd8944aa362bb4b55f
Nangcr <nangcr@gmail.com>
2021-11-08 11:35:33 +0800

    add argument

--- cmd/find-suffix/main.go
+++ cmd/find-suffix/main.go
@@ -7,18 +7,18 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) < 3 {
 		println("missing argument")
 		os.Exit(1)
 	}
 
-	regEx, err := regexp.Compile("^.+\\.(" + os.Args[1] + ")$")
+	regEx, err := regexp.Compile("^.+\\.(" + os.Args[2] + ")$")
 	if err != nil {
 		println(err)
 		os.Exit(1)
 	}
 
-	err = filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
+	err = filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error) error {
 		if err == nil && regEx.MatchString(info.Name()) {
 			println(path)
 		}
